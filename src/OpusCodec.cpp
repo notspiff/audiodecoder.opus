da6896416aa2c8a83350149225a47f2d9f0e0fa9
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2014-10-31 15:15:59 +0100

    rebranding. xbmc -> kodi

--- src/OpusCodec.cpp
+++ src/OpusCodec.cpp
@@ -18,12 +18,12 @@
  *
  */
 
-#include "xbmc/libXBMC_addon.h"
+#include "kodi/libXBMC_addon.h"
 
 extern "C" {
 #include <opus/opusfile.h>
-#include "xbmc/xbmc_audiodec_dll.h"
-#include "xbmc/AEChannelData.h"
+#include "kodi/kodi_audiodec_dll.h"
+#include "kodi/AEChannelData.h"
 #include <inttypes.h>
 
 ADDON::CHelper_libXBMC_addon *XBMC           = NULL;
